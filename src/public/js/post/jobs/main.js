fc4faa08df618087eb2d31b30d98bf958675f17e
Roberto0-0 <robertosilva88526@gmail.com>
2023-04-04 17:15:22 -0300

    job page settings

--- src/public/js/post/jobs/main.js
+++ src/public/js/post/jobs/main.js
@@ -1,6 +1,32 @@
 const date = document.getElementsByClassName("date")
 const salary = document.getElementsByClassName("salary")
+const li = document.querySelectorAll(".li")
+const text = document.querySelectorAll(".text")
+const btnseeMore = document.querySelectorAll(".btn-seeMore")
+
+var clicked = false
+
 const main = () => {
+  for(let j=0;j < li.length;j++) {
+    if(text[j].textContent.length > 250) {
+      btnseeMore[j].style.display = "block"
+      btnseeMore[j].onclick = () => {
+        if(!clicked) {
+          text[j].style.height = "100px"
+          btnseeMore[j].innerHTML = "...see more"
+          clicked = true
+        } else if(clicked) {
+          text[j].style.height = "auto"
+          btnseeMore[j].innerHTML = "see less"
+          clicked = false
+        }
+      }
+    } else if(text[j].textContent.length < 250) {
+      btnseeMore[j].style.display = "none"
+      text[j].style.height = "auto"
+    }
+  } 
+
   for(var i=0; i < date.length;i++) {
     date[i].innerHTML = `${dayjs(date[i].textContent).format("DD/MM/YY")}`
   }
