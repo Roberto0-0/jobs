04af3315f4bbb940b4cb0818a4eb1729dbe65b2d
Your Name <you@example.com>
2023-04-14 08:33:43 -0300

    someone settings

--- src/services/Like/read.ts
+++ src/services/Like/read.ts
@@ -2,25 +2,51 @@ import { UserRepository } from "../../repositories/UserRepository"
 import { LikeRepository } from "../../repositories/LikeRepository"
 import { PostRepository } from "../../repositories/PostRepository"
 import { CompanyRepository } from "../../repositories/CompanyRepository"
+import { Read } from "../Post/read"
+
+const users: any[] = []
 
 export class LikeRead {
     async execute(company_id: string) {
         const company = await CompanyRepository.find({
-            where: { id: company_id }
+            where: { id: company_id },
+            relations: {
+                post: true
+            }
         })
         
-        /*const like = await LikeRepository.findOne({
-            where: { postId: post_id }
+        
+        const posts = new Promise((resolve, reject) => {
+            company.map((companyValues) => {
+                companyValues.post.map( async (postValues) => {
+                    const post = await PostRepository.findOne({
+                        where: { id: postValues.id },
+                        relations: {
+                            like: true
+                        }
+                    })
+                    
+                    if(post.like) {
+                        post.like.map( async (likeValues) => {
+                            if(likeValues) {
+                                const user = await UserRepository.findOneBy({ id: likeValues.user_id })
+                                
+                                if(!user) {
+                                    return new Error("User not found!")
+                                }
+                                
+                                resolve(user)
+                            }
+                        })
+                    }
+                })
+            })
         })
         
-        if(!like) {
-            return new Error("Like not fould!")
-        }
         
-        const users = await UserRepository.find({
-            where: { id: like.userId }
-        })*/
-        
-        return company
+        return posts.then((data) => {
+            return data
+        })
     }
+    
 }
