e10173582f9a3b948ce7f15c62250785f1c06b84
Your Name <you@example.com>
2023-04-21 09:08:34 -0300

    someone settings

--- src/public/js/user/register/main.js
+++ src/public/js/user/register/main.js
@@ -2,18 +2,14 @@ const password = document.querySelectorAll(".password")
 const eyeButton = document.querySelectorAll(".ph-eye")
 const error_message = document.querySelectorAll(".error_message")
 
-const main = () => {
-    setTimeout(() => {
-        for(var i=0;i < error_message.length;i++) {
-            error_message[i].style.display = "none"
-        }
-    }, 5000)
+console.log(eyeButton)
 
+const main = () => {
     for (let i = 0; i < password.length; i++) {
         eyeButton[i].onclick = () => {
             if(password[i].type == "password") {
                 password[i].type = "text"
-                eyeButton[i].classList = "ph-eye-closed"
+                eyeButton[i].classList = "ph ph-eye-closed"
             } else {
                 if(password[i].type == "text") {
                     password[i].type = "password"
@@ -22,6 +18,12 @@ const main = () => {
             }
         }   
     }
+    
+    setTimeout(() => {
+        for(let i=0;i < error_message.length;i++) {
+            error_message[i].style.display = "none"
+        }
+    }, 5000)
 }
 
 main()
