066445253320ba8117afaa76c106ad7b9ff94acc
Roberto0-0 <robertosilva88526@gmail.com>
2023-05-22 19:30:27 -0300

    someone settings

--- src/public/js/menu/main.js
+++ src/public/js/menu/main.js
@@ -1,6 +1,11 @@
 const menu = document.querySelector(".menu")
 const nav = document.querySelector("nav")
+const mainRemoveClass = document.querySelector("main")
 
 menu.onclick = () => {
     nav.classList.toggle('active')
+}
+
+mainRemoveClass.onclick = () => {
+    nav.classList.remove('active')
 }
--- src/public/js/resume/userSentResume/main.js
+++ src/public/js/resume/userSentResume/main.js
@@ -1,10 +1,18 @@
 const notification = document.querySelector('.notification')
+const dots = document.querySelectorAll(".dots")
+const submenu = document.querySelectorAll(".submenu")
 
 const main = () => {
     if(notification) {
         setTimeout(() => {
             notification.style.display = "none"
         }, 3000)
+    }
+
+    for (let i=0;i < dots.length;i++) {
+        dots[i].onclick = () => {
+            submenu[i].classList.toggle("active")
+        }        
     }
 }
 
