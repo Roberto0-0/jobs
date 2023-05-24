db533666ef862483a81ffeeeda485f0a37f1aaae
Roberto0-0 <robertosilva88526@gmail.com>
2023-05-24 12:22:08 -0300

    someone settings

--- src/public/js/themes/main.js
+++ src/public/js/themes/main.js
@@ -1,16 +1,25 @@
 const changeColor = document.querySelector(".change-color")
 const moon = document.querySelector(".ph-moon")
 const body = document.querySelector("body")
+const home_illustration = document.querySelector(".home-illustration")
+const dashboard_illustration = document.querySelector(".dashboard-illustration")
+const compnay_illustration = document.querySelector(".company-illustration")
 
 const themes = () => {
    var result = window.localStorage.getItem("themes")
     if(result && result === "dark") {
         moon.classList = "ph ph-sun" 
         body.classList.toggle("active")
+        if(home_illustration ) { home_illustration .src = "/assets/jobs/undraw_file_searching_re_3evy_dark.svg" }
+        if(dashboard_illustration) { dashboard_illustration.src = "/assets/jobs/undraw_done_checking_re_6vyx_dark.svg" }
+        if(compnay_illustration) { compnay_illustration.src = "/assets/jobs/undraw_financial_data_re_p0fl_dark.svg" }
     }
     if(result && result === "light") {
         moon.classList = "ph ph-moon"
         body.classList.remove("active")
+        if(home_illustration ) { home_illustration .src = "/assets/jobs/undraw_file_searching_re_3evy.svg" }
+        if(dashboard_illustration) { dashboard_illustration.src = "/assets/jobs/undraw_done_checking_re_6vyx.svg" }
+        if(compnay_illustration) { compnay_illustration.src = "/assets/jobs/undraw_financial_data_re_p0fl.svg" }
     }
 
     changeColor.onclick = (e) => {
@@ -18,10 +27,16 @@ const themes = () => {
             moon.classList = "ph ph-sun"
             body.classList.toggle("active")
             window.localStorage.setItem("themes", "dark")
+            if(home_illustration ) { home_illustration .src = "/assets/jobs/undraw_file_searching_re_3evy_dark.svg" }
+            if(dashboard_illustration) { dashboard_illustration.src = "/assets/jobs/undraw_done_checking_re_6vyx_dark.svg" }
+            if(compnay_illustration) { compnay_illustration.src = "/assets/jobs/undraw_financial_data_re_p0fl_dark.svg" }
         } else {
             moon.classList = "ph ph-moon"
             body.classList.remove("active")
             window.localStorage.setItem("themes", "light")
+            if(home_illustration ) { home_illustration .src = "/assets/jobs/undraw_file_searching_re_3evy.svg" }
+            if(dashboard_illustration) { dashboard_illustration.src = "/assets/jobs/undraw_done_checking_re_6vyx.svg" }
+            if(compnay_illustration) { compnay_illustration.src = "/assets/jobs/undraw_financial_data_re_p0fl.svg" }
         }
     }
 }
